1a119d6072d1b887da04e9e1f5902cf6324b210b
khyshant <anth.blanchard@gmail.com>
2020-08-30 02:33:57 +0200

    initial entity files

---
+++ src/Entity/Brand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BrandRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BrandRepository::class)
+ */
+class Brand
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
