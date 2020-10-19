664aa5961fb737728b983aa902c9796040f59ee1
khyshant <anth.blanchard@gmail.com>
2020-10-19 21:47:59 +0200

    add voter

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,12 +3,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Customer;
 use App\Entity\User;
+use App\Repository\CustomerRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Security\Core\Security as SecurityAlias;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
 
 
 /**
@@ -18,6 +27,15 @@ use Symfony\Component\Serializer\SerializerInterface;
  */
 class UserController
 {
+    /**
+     * @var SecurityAlias
+     */
+    private $security;
+
+    public function __construct(SecurityAlias $security)
+    {
+        $this->security = $security;
+    }
 
     /**
      * @Route(name="api_users_listing", methods={"GET"})
@@ -28,23 +46,27 @@ class UserController
     public function listing(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
     {
 
+        $customer = $this->security->getUser();
+        dump($customer);
         return new JsonResponse($serializer->serialize($userRepository->findAll(),"json", ["groups"=> "get"]),
         JsonResponse::HTTP_OK,
-        '',
+        [],
         true);
     }
 
     /**
-     * @Route("/{id}" name="api_users_item", methods={"GET"})
+     * @Route("/{id}", name="api_users_item", methods={"GET"})
      * @param User $user
      * @param SerializerInterface $serializer
      * @return JsonResponse
      */
     public function item(User $user, SerializerInterface $serializer): JsonResponse
     {
+        $customer = $this->security->getUser();
+        dump($customer);
         return new JsonResponse($serializer->serialize($user,"json", ["groups"=> "get"]),
             JsonResponse::HTTP_OK,
-            '',
+            [],
             true);
     }
 
@@ -53,18 +75,69 @@ class UserController
      * @param Request $request
      * @param SerializerInterface $serializer
      * @param EntityManagerInterface $entityManager
+     * @param UrlGeneratorInterface $urlGenerator
      * @return JsonResponse
      */
-    public function user(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    public function post(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator): JsonResponse
     {
+        $customer = $this->security->getUser();
+        dump($customer);
+        /** @var $user User*/
         $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        // a supprimer quand j'aurais le retour du token
+        $user->setCustomer($entityManager->getRepository(Customer::class)->findOneBy([]));
         $entityManager->persist($user);
         $entityManager->flush();
 
         return new JsonResponse(
-            $serializer->serialize($user->findAll(),"json", ["groups"=> "get"]),
+            $serializer->serialize($user,"json", ["groups"=> "get"]),
             JsonResponse::HTTP_CREATED,
-            '',
+            ["Location" => $urlGenerator->generate("api_users_item", ["id" => $user->getId()])],
             true);
+    }
+
+    /**
+     * @Route("/{id}",name="api_users_item_modify", methods={"PUT"})
+     * @IsGranted("edit",subject="user")
+
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
+     */
+    public function put(User $user, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $customer = $this->security->getUser();
+        dump($customer);
+        /** @var $user User*/
+        $serializer->deserialize($request->getContent(), User::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE =>$user]);
+        // a supprimer quand j'aurais le retour du token
+        $user->setCustomer($entityManager->getRepository(Customer::class)->findOneBy([]));
+
+        $entityManager->flush();
+
+        return new JsonResponse(
+            null,
+            JsonResponse::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @Route("/{id}",name="api_users_item_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $entityManager
+     * @return JsonResponse
+     */
+    public function delete(User $user, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $customer = $this->security->getUser();
+        dump($customer);
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return new JsonResponse(
+            null,
+            JsonResponse::HTTP_NO_CONTENT);
     }
 }
