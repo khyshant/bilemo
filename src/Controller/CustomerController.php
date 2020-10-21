6f45a89eed41f814497205890ba4ed09d057032f
khyshant <anth.blanchard@gmail.com>
2020-10-21 21:34:11 +0200

    addbazinga_hateoas

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Class BrandController
+ * @package App\Controller
+ * @Route("/api/customers")
+ */
+class CustomerController
+{
+
+}
